f73515d203d03e7df5d741ad5c72d31fa967c031
dcmallett <dcmallett28@gmail.com>
2022-08-15 16:37:45 +0100

    completed python game

--- game.py
+++ game.py
@@ -1,11 +1,26 @@
-#Python youtube tutorial timestamp: 
+#Python youtube tutorial timestamp: 1:42:32 selecting to print the round results.
+
+from itertools import count
+from random import randint
+
 
 game_running = True
 
+#empty list for our results
+game_results = []
+
+def calculate_monster_attack(attack_min, attack_max):
+    return randint(attack_min, attack_max)
+
+
+def game_ends(winner_name):
+    print(f'{winner_name} has won the game')
+
 while game_running == True:
+    counter = 0
     new_round = True
-    player = {'name': 'Dan', 'attack': 15, 'heal': 60, 'health': 100}
-    monster = { 'name': 'bob', 'attack': 17, 'health': 100 }
+    player = {'name': 'Dan', 'attack': 10, 'heal': 60, 'health': 100}
+    monster = { 'name': 'bob', 'attack_min': 10, 'attack_max': 20, 'health': 100 }
 
     #this * means we want to print the --- before the start of the game
     print('---' * 7)
@@ -17,7 +32,7 @@ while game_running == True:
     print(monster['name'] + ' has ' + str(monster['health']) + ' health')
    
     while new_round == True:
-
+        counter = counter + 1
         player_won = False
         monster_won = False
 
@@ -26,6 +41,7 @@ while game_running == True:
         print('1) Attack')
         print('2) Heal')
         print('3) Exit game')
+        print('4) Show the results')
 
         #input method allows the user to select the input they need to start the game
         player_choice = input()
@@ -36,19 +52,23 @@ while game_running == True:
             if monster['health'] <= 0:
                 player_won = True
             else:
-                player['health'] = player['health'] - monster['attack']
+                player['health'] = player['health'] - calculate_monster_attack(monster['attack_min'], monster['attack_max'])
             if player['health'] <= 0:
                 monster_won = True
 
         elif player_choice == '2':
             player['health'] = player['health'] + player['heal']
-            player['health'] = player['health'] - monster['attack']
+            player['health'] = player['health'] - calculate_monster_attack(monster['attack_min'], monster['attack_max'])
             if player['health'] <= 0:
                 monster_won = True
 
         elif player_choice == '3':
             new_round = False
             game_running = False
+        
+        elif player_choice == '4':
+            for result in game_results:
+                print(result)
         else: 
             print('Invalid input')
         if player_won == False and monster_won == False:
@@ -56,9 +76,13 @@ while game_running == True:
             print(monster['name'] + ' has ' + str(monster['health']) + ' left')
 
         elif player_won:
-            print(player['name'] + ' won')
+            game_ends(player['name'])
+            round_results = {'name': player['name'], 'health': player['health'], 'Number of rounds': counter}
+            game_results.append(round_results)
             new_round = False
 
         elif monster_won:
-            print('The monster won')
+            game_ends(monster['name'])
+            round_results = {'name': monster['name'], 'health': monster['health'], 'Number of rounds': counter}
+            game_results.append(round_results)
             new_round = False
